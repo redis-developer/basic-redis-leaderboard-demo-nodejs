efbb5c3903c371c4252e19569a07ad935172b610
Ajeet Singh Raina, Docker Captain, RedisLabs <ajeetraina@gmail.com>
2021-06-17 13:09:35 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
